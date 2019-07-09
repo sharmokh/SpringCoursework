1ae9749915b613b0f2034a6ac866950effc20b1e
Steve <steve.sharmokh@gmail.com>
2019-07-09 11:45:06 -0700

    Refactored Receipe Setters for Bi-Directional Relationship

--- recipe-app/src/main/java/com/sharmokh/recipeapp/model/Recipe.java
+++ recipe-app/src/main/java/com/sharmokh/recipeapp/model/Recipe.java
@@ -100,6 +100,12 @@ public class Recipe {
         this.url = url;
     }
 
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
     public Set<Ingredient> getIngredients() {
         return ingredients;
     }
@@ -138,6 +144,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Category> getCategories() {
